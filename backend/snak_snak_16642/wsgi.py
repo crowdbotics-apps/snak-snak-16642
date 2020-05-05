260db88dd88d01e803222379e758e17958754b20
Crowdbotics <team@crowdbotics.com>
2020-05-05 23:12:55 +0000

    Update models

--- backend/snak_snak_16642/wsgi.py
+++ backend/snak_snak_16642/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snak_snak_16642.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snak_snak_16642.settings")
 
 application = get_wsgi_application()
