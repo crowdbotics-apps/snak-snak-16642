55af6de143872600f135adf9a9f07ac257c8a8bb
Geri <gerienesi@gmail.com>
2021-01-14 23:55:56 +0100

    invitation, feedback, notification

--- backend/users/api/v1/urls.py
+++ backend/users/api/v1/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from users.api.v1.viewsets import ProfileUpdateView, ProfilePublicView, UserSettingsView, UserSearch
+from users.api.v1.viewsets import ProfileUpdateView, ProfilePublicView, UserSettingsView, UserSearch, \
+    UserNotificationId, SendUserNotification, AcceptInvitation, EditUserInvitation, GetInvitationDetails, Feedback
 
 urlpatterns = [
     path("profile/", ProfileUpdateView.as_view(), name="user_profile"),
     path("public/profile/<int:pk>/", ProfilePublicView.as_view(), name="user_public_profile"),
     path("settings/", UserSettingsView.as_view(), name="user_settings"),
-    path("search/", UserSearch.as_view(), name="user_search")
+    path("search/", UserSearch.as_view(), name="user_search"),
+    path("notification/", UserNotificationId.as_view(), name="notification"),
+    path("send-notification/", SendUserNotification.as_view(), name="send_notification"),
+    path("accept-invitation/", AcceptInvitation.as_view(), name="accept-invitation"),
+    path("edit-invitation/", EditUserInvitation.as_view(), name="edit-invitation"),
+    path("get-invitation-details/", GetInvitationDetails.as_view(), name="get-invitation-details"),
+    path("feedback/", Feedback.as_view(), name="feedback")
 ]
--- backend/users/api/v1/viewsets.py
+++ backend/users/api/v1/viewsets.py
@@ -1,15 +1,18 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.http import Http404
+from onesignal_sdk.error import OneSignalHTTPError
 from rest_framework import status, filters
-from rest_framework.generics import ListAPIView
+from onesignal_sdk.client import Client
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.api.v1.serializers import UserProfileSerializer, UserSettingsSerializer
-from users.models import Settings, JobFields
+from users.api.v1.serializers import UserProfileSerializer, UserSettingsSerializer, UserInvitation
+from users.models import Settings, JobFields, Invitations
 
 User = get_user_model()
+client = Client(app_id=settings.APP_ID, rest_api_key=settings.REST_API_KEY, user_auth_key=settings.USER_AUTH_KEY)
 
 
 class ProfileUpdateView(APIView):
@@ -79,6 +82,7 @@ class UserSettingsView(APIView):
 
 
 class UserSearch(APIView):
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
         user_jobs = JobFields.objects.filter(job_field__in=request.data.get('jobs'))
@@ -86,3 +90,129 @@ class UserSearch(APIView):
         users = User.objects.filter(id__in=users_id)
         serializer = UserProfileSerializer(users, many=True)
         return Response(serializer.data)
+
+
+class UserNotificationId(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user_notify_id = request.data['user_notify_id']
+        user = request.user
+        user.notify_id = user_notify_id
+        user.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class SendUserNotification(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        invited_user = request.data['invited_user_id']
+        place = request.data['place']
+        date = request.data['date']
+        time = request.data['time']
+        message = request.data['message']
+        check_user = User.objects.filter(id=invited_user)
+        if check_user.exists():
+            user_notification_id = check_user[0].notify_id
+            notification_body = {
+                "include_player_ids": [user_notification_id],
+                "contents": {"en": "You have received a new invitation."}
+            }
+            try:
+                client.send_notification(notification_body)
+                inv = Invitations.objects.create(
+                    invited_user=check_user[0],
+                    user=request.user,
+                    place=place,
+                    date=date,
+                    time=time,
+                    message=message
+                )
+
+                serializer = UserInvitation(inv)
+                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            except OneSignalHTTPError as e:
+                return Response(e.http_response.json(), status=e.status_code)
+        return Response({'msg': 'Invited user does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class EditUserInvitation(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        invitation_id = request.data['invitation_id']
+        place = request.data['place']
+        date = request.data['date']
+        time = request.data['time']
+        check_inv = Invitations.objects.filter(id=invitation_id)
+        if check_inv.exists():
+            inv = check_inv[0]
+            inv.place = place
+            inv.date = date
+            inv.time = time
+            inv.save()
+            invitation_user_id = inv.user.notify_id if inv.user != request.user else inv.invited_user.notify_id
+            notification_body = {
+                "include_player_ids": [invitation_user_id],
+                "contents": {"en": "You have an invitation update."}
+            }
+            try:
+                client.send_notification(notification_body)
+                serializer = UserInvitation(inv)
+                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            except OneSignalHTTPError as e:
+                return Response(e.http_response.json(), status=e.status_code)
+        return Response({'msg': 'Invitation does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class GetInvitationDetails(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        invitation_id = request.data['invitation_id']
+        check_inv = Invitations.objects.filter(id=invitation_id)
+        if check_inv.exists():
+            serializer = UserInvitation(check_inv[0])
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response({'msg': 'Invitation does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class AcceptInvitation(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        invitation_id = request.data['invitation_id']
+
+        inv = Invitations.objects.filter(id=invitation_id)
+
+        if inv.exists():
+            invitation = inv[0]
+            invitation.status = True
+            invitation.save()
+
+            notification_body = {
+                "include_player_ids": [invitation.user.notify_id],
+                "contents": {"en": "{0} has accepted your invitation.".format(invitation.invited_user.get_full_name())}
+            }
+            try:
+                client.send_notification(notification_body)
+                return Response({'msg': 'Invitations accepted', 'room_id': invitation.room_id}, status=status.HTTP_201_CREATED)
+            except OneSignalHTTPError as e:
+                return Response(e.http_response.json(), status=e.status_code)
+
+
+class Feedback(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        invitation_id = request.data['invitation_id']
+        inv = Invitations.objects.filter(id=invitation_id)
+
+        if inv.exists():
+            invitation = inv[0]
+            invitation.feedback = True
+            invitation.save()
+
+            return Response(status=status.HTTP_201_CREATED)
+        return Response({'msg': 'Invitation does not exists'}, status=status.HTTP_404_NOT_FOUND)
