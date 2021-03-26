e0af84786b57f8812e2f06851bc9a133e1a2745f
Nabeel Akhtar <67922640+nabeelakhtar2020@users.noreply.github.com>
2021-03-27 01:25:00 +0500

    Update AWS Integration

--- backend/snak_snak_16642/settings.py
+++ backend/snak_snak_16642/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", default=None)
 
 ALLOWED_HOSTS = env.list("HOST", default=["*"])
 SITE_ID = 1
@@ -157,28 +157,35 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication'
     ]
 }
+STATIC_URL = '/static/'
+
+# S3_FILE_STORAGE = env.bool("S3_FILE_STORAGE", True)
 
 # AWS S3 STORAGE START
-AWS_ACCESS_KEY_ID = 'AKIAIEB2U3X7RJW6QV3Q'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'snack-snack-dev'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_STATIC_LOCATION = 'static'
+# AWS_ACCESS_KEY_ID = 'AKIAIEB2U3X7RJW6QV3Q'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'snack-snack-dev'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_STATIC_LOCATION = 'static'
 # AWS S3 STORAGE END
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_STORAGE = 'snak_snak_16642.storage_backends.StaticStorage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
-DEFAULT_FILE_STORAGE = 'snak_snak_16642.storage_backends.PublicMediaStorage'
 
-AWS_PRIVATE_MEDIA_LOCATION = 'media/private/profile_images'
-PRIVATE_FILE_STORAGE = 'snak_snak_16642.storage_backends.PrivateMediaStorage'
+# AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+
+# if S3_FILE_STORAGE:
+#     DEFAULT_FILE_STORAGE = 'snak_snak_16642.storage_backends.PublicMediaStorage'
+#     STATICFILES_STORAGE = 'snak_snak_16642.storage_backends.StaticStorage'
+#     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+#
+# AWS_PRIVATE_MEDIA_LOCATION = 'media/private/profile_images'
+# PRIVATE_FILE_STORAGE = 'snak_snak_16642.storage_backends.PrivateMediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -188,8 +195,8 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-ACCOUNT_CONFIRM_EMAIL_ON_GET = True
-ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+ACCOUNT_CONFIRM_EMAIL_ON_GET = False
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = False
 ACCOUNT_UNIQUE_EMAIL = True
 LOGIN_REDIRECT_URL = "users:redirect"
 
@@ -230,3 +237,22 @@ if DEBUG:
 APP_ID = 'e304774b-9c6f-434e-913b-ba24addb5246'
 REST_API_KEY = 'ZTRiN2Q3NzYtYjYyZC00YzQ4LWJkNTEtNmIyMmQwZmVjZDUw'
 USER_AUTH_KEY = 'ZDgzNWI5NDMtN2VhNi00NWNkLWIyOTctNTcyYWE2NzEzMjM2'
+
+
+S3_FILE_STORAGE = env.bool("S3_FILE_STORAGE", True)
+AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY", "")
+AWS_AUTO_CREATE_BUCKET = True
+AWS_S3_FILE_OVERWRITE = True
+AWS_S3_REGION_NAME = env.str("AWS_STORAGE_REGION", "us-east-1")
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME", "")
+AWS_DEFAULT_ACL = 'private'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+if S3_FILE_STORAGE:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+try:
+    from snak_snak_16642.local_settings import *
+except:
+    pass
--- backend/snak_snak_16642/storage_backends.py
+++ backend/snak_snak_16642/storage_backends.py
@@ -3,14 +3,16 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.AWS_STATIC_LOCATION
+    location = settings.STATIC_URL
+
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = settings.AWS_PUBLIC_MEDIA_LOCATION
+    # location = settings.AWS_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
 
+
 class PrivateMediaStorage(S3Boto3Storage):
-    location = settings.AWS_PRIVATE_MEDIA_LOCATION
+    # location = settings.AWS_PRIVATE_MEDIA_LOCATION
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
